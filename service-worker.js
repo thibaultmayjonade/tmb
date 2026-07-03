392ffa08cf3a3f9e9520424fac10c998b054b8f1
thibaultmayjonade <thibault.mayjonade@gmail.com>
2026-07-03 21:01:54 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'tmb-golgothes-2026-v11-iconfix';
+const CACHE = 'tmb-golgothes-2026-v12-effort';
 const CORE = ['./','index.html','style.css','script.js','data.js','manifest.webmanifest','icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(()=>self.skipWaiting()));
